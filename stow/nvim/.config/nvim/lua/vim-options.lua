edbc316e1523c5bb8dd6fccd3d94bb550e1bdb59
Aness Chelfat <ns.chlfat@gmail.com>
2026-02-23 15:16:23 +0100

    Remove duplicates

--- stow/nvim/.config/nvim/lua/vim-options.lua
+++ stow/nvim/.config/nvim/lua/vim-options.lua
@@ -1,9 +1,14 @@
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.opt.scrolloff = 12
+vim.opt.smartindent = true
 vim.opt.swapfile = false
 vim.opt.termguicolors = true
 
@@ -14,10 +19,6 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.opt.scrolloff = 12
-vim.opt.smartindent = true
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
